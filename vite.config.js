7d7b2e2efe314fb36315f1201471c5c5ef3e7082
Mahima <aroramahima89@gmail.com>
2025-10-31 18:59:33 +0530

    port issue fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",        // 👈 Expose the server to external hosts
-    port: 5178,             // 👈 Use the same port Vite picks
+    port: 5181,             // 👈 Use the same port Vite picks
     strictPort: true,       // 👈 Keep the same port (optional)
     allowedHosts: ["*"],    // 👈 Let CodeSandbox proxy through
     cors: true,             // 👈 Avoid CORS blocks in iframe
