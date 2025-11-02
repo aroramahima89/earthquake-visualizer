522695fd174c0e1a64976f02b5f681b1c2499223
Mahima <62737665+aroramahima89@users.noreply.github.com>
2025-11-02 17:00:45 +0000

    vite config fixes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: "0.0.0.0", // 👈 Expose the server to external hosts
     port: 5178, // 👈 Use the same port Vite picks
     strictPort: true, // 👈 Keep the same port (optional)
-    allowedHosts: ["*"], // 👈 Let CodeSandbox proxy through
+    allowedHosts: ["4mfrzk-5178.csb.app"], // 👈 Let CodeSandbox proxy through
     cors: true, // 👈 Avoid CORS blocks in iframe
   },
   resolve: {
